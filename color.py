bbbd7b1cb3623176321b21e2609f0b9b4f6c7887
UCSB <ucsb@Biks-MacBook-Air.local>
2019-06-05 21:05:34 -0700

    trying some other stuff on RGB manipulation

--- color.py
+++ color.py
@@ -9,12 +9,15 @@ import numpy as np
 
 def main (input_file):
 	image = misc.imread(input_file)
-	
+	print (image.shape)
 	factor = 2
 
 	red = image[:,:,0]
+
 	green = image[:,:,1]
+
 	blue = image[:,:,2]
+	
 
 	red_frac = red/factor
 	green_frac = green/factor
@@ -27,8 +30,13 @@ def main (input_file):
 
 	reconstruct = (factor * red) + (factor * green) + (factor * blue)
 
-	matplotlib.pyplot.imshow(reconstruct)
-	matplotlib.pyplot.show()
+	fileName = input_file.split('.')[0]
+
+	fileNameSave = fileName + "_copy.bmp"
+	misc.imsave(fileNameSave, reconstruct)
+
+	#matplotlib.pyplot.imshow(reconstruct, cmap)
+	# matplotlib.pyplot.show()
 
 	# matplotlib.pyplot.imshow(grey, cmap = matplotlib.cm.Greys_r)
 	# matplotlib.pyplot.show()
